a918d5d72dd384a9ac117e5361532cbab6288ba9
yagmurozer <yagmur31323@hotmail.com>
2025-04-25 20:19:24 +0300

    Odev1

---
+++ app/src/main/java/com/example/odev1/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.odev1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.odev1.ui.theme.Odev1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Odev1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Odev1Theme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/odev1/degiskenler/Odev1.kt
@@ -0,0 +1,99 @@
+package com.example.odev1.degiskenler
+
+fun main(){
+
+    val sehir = "İzmir"
+    val ulke = "Türkiye"
+    val telefon = 1123546
+    val postaKodu = 35452
+    val email = "sdmvldskvm@gmail.com"
+    val meslek = "bilgisayar mühendisi"
+    val stokMiktari = 1354688
+    val musteriAdi = "ayşe"
+    val bakiye = 13514656
+    val dogumGunu = "13.02.2004"
+    val maas = 22000
+    val medeniDurum = "bekar"
+    val urunYorum = "beğendim"
+    val odemeTarihi = "2025-04-25"
+    val odeme = 299.99
+    val siparisAdeti = 45
+    val arabaModeli = "Toyota Corolla"
+    val kitapAdi = "içimizdeki şeytan"
+    val yayinlamaTarihi = "2020-09-10"
+    val indirimMiktari = 25
+    val odaSayisi = 2
+    val enlem = 38.4237
+    val boylam = 27.1428
+    val urunAdi = "kulaklık"
+    val yemekFiyati = 250.5
+    val marka = "Bosch"
+    val muzikAdi = "Tanrıdan diledim"
+    val videoSuresi = "12:30"
+    val urunPuani = 3.4
+    val resimAdi = "manzara.jpg"
+    val dosyaFormati = "PNG"
+    val renk = "Mavi"
+    val renkKodu = "#0000FF"
+    val telefonModeli = "Iphone 15"
+    val ekranBoyutu = "15 cm"
+    val agirlik = "10 kg"
+    val ulusalGun = "23 Nisan"
+    val tatilGunu = "Pazartesi"
+    val rezervasyonTarihi = "2025-08-10"
+    val sokakAdi = "Kavak Sokak"
+    val otobusHatti = "708"
+    val kalanDakika = 34
+    val takipKodu = "vdbdfb5464621"
+    val kuponSuresi = "15 gün"
+    val kuponKodu = "indirim30"
+    val faturaAdresi = "Kasımpaşa Mahallesi Menderes/İzmir"
+
+    println("Şehir: $sehir")
+    println("Ülke: $ulke")
+    println("Telefon: $telefon")
+    println("Posta Kodu: $postaKodu")
+    println("Email: $email")
+    println("Meslek: $meslek")
+    println("Stok Miktarı: $stokMiktari")
+    println("Müşteri Adı: $musteriAdi")
+    println("Bakiye: $bakiye")
+    println("Doğum Günü: $dogumGunu")
+    println("Maaş: $maas")
+    println("Medeni Durum: $medeniDurum")
+    println("Ürün Yorum: $urunYorum")
+    println("Ödeme Tarihi: $odemeTarihi")
+    println("Ödeme: $odeme")
+    println("Sipariş Adeti: $siparisAdeti")
+    println("Araba Modeli: $arabaModeli")
+    println("Kitap Adı: $kitapAdi")
+    println("Yayınlama Tarihi: $yayinlamaTarihi")
+    println("İndirim Miktarı: $indirimMiktari")
+    println("Oda Sayısı: $odaSayisi")
+    println("Enlem: $enlem")
+    println("Boylam: $boylam")
+    println("Ürün Adı: $urunAdi")
+    println("Yemek Fiyatı: $yemekFiyati")
+    println("Marka: $marka")
+    println("Müzik Adı: $muzikAdi")
+    println("Video Süresi: $videoSuresi")
+    println("Ürün Puanı: $urunPuani")
+    println("Resim Adı: $resimAdi")
+    println("Dosya Formatı: $dosyaFormati")
+    println("Renk: $renk")
+    println("Renk Kodu: $renkKodu")
+    println("Telefon Modeli: $telefonModeli")
+    println("Ekran Boyutu: $ekranBoyutu")
+    println("Ağırlık: $agirlik")
+    println("Ulusal Gün: $ulusalGun")
+    println("Tatil Günü: $tatilGunu")
+    println("Rezervasyon Tarihi: $rezervasyonTarihi")
+    println("Sokak Adı: $sokakAdi")
+    println("Otobüs Hattı: $otobusHatti")
+    println("Kalan Dakika: $kalanDakika")
+    println("Takip Kodu: $takipKodu")
+    println("Kupon Süresi: $kuponSuresi")
+    println("Kupon Kodu: $kuponKodu")
+    println("Fatura Adresi: $faturaAdresi")
+
+}
